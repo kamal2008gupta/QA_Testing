52b9c7b719f4ff78040dfa8ba1d6e26b7e8fc8d5
kamal <kamal2008gupta@gmail.com>
2024-12-26 10:47:15 -0800

    firs

--- src/dec30/conditional_operator.java
+++ src/dec30/conditional_operator.java
@@ -2,14 +2,24 @@ package dec30;
 
 public class conditional_operator {
     public static void main (String [] args) {
-      int  a=30,b=20;
+     /* int  a=30,b=20;
         String max=a>b? "a is bigger": "b is smaller";
-        System.out.println("bigger numnber is" + max);
+        System.out.println("bigger numnber is" + max);*/
 
       /*  int x = 10;
         int y = 25;
         int z = x + y;
 
         System.out.println("Sum of x+y = "+ z);*/
+
+        // check grade of the student
+
+        int score=40;
+        String grade = (score >= 90) ? "A" :(score >= 80) ? "B" : (score>= 70) ? "C" : (score >= 60) ? "D" : (score >= 50) ? "E" :"F";
+        System.out.printf("your grade is %s", grade);
+        System.out.println("your grade is " +grade);
+
+
+
     }
 }
