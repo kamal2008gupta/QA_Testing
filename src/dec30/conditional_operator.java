fac8c2bf34646932976807c518ebe4d003d22bc1
kamal <kamal2008gupta@gmail.com>
2024-12-25 21:43:25 -0800

    firs

---
+++ src/dec30/conditional_operator.java
@@ -0,0 +1,15 @@
+package dec30;
+
+public class conditional_operator {
+    public static void main (String [] args) {
+      int  a=30,b=20;
+        String max=a>b? "a is bigger": "b is smaller";
+        System.out.println("bigger numnber is" + max);
+
+      /*  int x = 10;
+        int y = 25;
+        int z = x + y;
+
+        System.out.println("Sum of x+y = "+ z);*/
+    }
+}
---
+++ src/dec30/helloworld.java
@@ -0,0 +1,7 @@
+package dec30;
+
+public class helloworld {
+    public static void main(String[] args){
+        System.out.println("hi hello");
+ }
+}
